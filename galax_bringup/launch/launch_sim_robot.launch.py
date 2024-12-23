e03bbf8cfc293f3ab620a820d3a17a2996bd3fae
Josep Rueda <rueda_999@hotmail.com>
2024-12-23 16:08:47 +0100

    fixed driting

--- galax_bringup/launch/launch_sim_robot.launch.py
+++ galax_bringup/launch/launch_sim_robot.launch.py
@@ -49,10 +49,13 @@ def generate_launch_description():
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={
+                'world': os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world'),
+                'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file
+            }.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
